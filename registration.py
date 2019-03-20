0975190a9dd2fbd1fee7c8399afd846e45226089
Quoc <quocble@gmail.com>
2019-03-19 18:22:52 -0700

    free QDEX

--- registration.py
+++ registration.py
@@ -5,10 +5,10 @@ import os
 def register_user(name, owner_key, registrar, referrer):
     rpc_url = os.environ.get("RPC_URL","ws://testnet-02.quantachain.io:8090")
     keys = [os.environ.get("KEY")]
-
+    free_QDEX = float(os.environ.get("FREE_QDEX"))
     conn = BitShares(rpc_url, key_store=storage.InRamPlainKeyStore(), keys = keys)
-    return conn.create_account(name, registrar, referrer, 100, owner_key, owner_key, owner_key)
-
+    account_tx = conn.create_account(name, registrar, referrer, 100, owner_key, owner_key, owner_key)
+    conn.transfer(name, free_QDEX, "QDEX", "free QDEX", registrar)
 
 #res = register_user("dead02", "QA4yeoMtbdDQeSh92HahSg1GPmh1yxg8Em1qRV94ZwJXvSu1huyq","1.2.15","1.2.15")
 #print(res)
