cdeec36e97a71a8d62de05d3a3f3c3694f6a6cfd
Quoc <quocble@gmail.com>
2019-04-16 18:38:52 -0700

    publish as latest

--- build_docker.sh
+++ build_docker.sh
@@ -5,3 +5,5 @@ echo building $IMG
 docker build -t $IMG .
 docker tag $IMG quantalabs/$IMG
 docker push quantalabs/$IMG
+docker tag $IMG quantalabs/quanta-accounthist-api:latest
+docker push quantalabs/quanta-accounthist-api:latest
